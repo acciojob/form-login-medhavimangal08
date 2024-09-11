ae46598e2b0c1ca31697fdf3af542448aea891b3
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-11 23:46:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
-    //Write your code here
+    // Select the input fields for first and last names
+    let textInput = document.querySelectorAll('input[type="text"]');
+    
+    // Prevent the default form submission behavior
+    event.preventDefault();
 
+    // Display an alert with the full name
+    alert(textInput[0].value + " " + textInput[1].value);
 }
